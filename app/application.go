0b1a4c404b19847bf8b492887311a91651bb077b
sabouaram <salimbouaram12@gmail.com>
2021-06-23 16:47:48 +0200

    Basic attachment feature

--- app/application.go
+++ app/application.go
@@ -2,7 +2,8 @@ package app
 
 import (
 	"context"
-	"fmt"
+	"encoding/base64"
+	"io/ioutil"
 	"log"
 
 	"github.com/sabouaram/reporting-reader/config"
@@ -25,42 +26,65 @@ func (a *Application) StartApp() {
 		log.Printf("Gmail Backend Authorized HTTP Client Service Created Successfully")
 		// Listing User messages based on a defined Query
 		req := gmailService.Users.Messages.List("me").Q("from: :is:unread Has:attachment")
-
 		r, err := req.Do()
 		if err != nil {
 			log.Fatalf("Unable to retrieve messages: %v", err)
 		}
 		log.Printf("Processing %v Unreaded mail with attachment...\n", len(r.Messages))
 		// Processing Unreaded mails with attachement
-		msgs := []*gmail.Message{}
 		for _, v := range r.Messages {
-			msg, err := gmailService.Users.Messages.Get("me", v.Id).Do()
+			msg, err := gmailService.Users.Messages.Get(a.Config.Username, v.Id).Do()
 			if err != nil {
 				log.Fatal("Error while getting Unreaded messages")
 			}
-			//log.Println(msg)
-			msgs = append(msgs, msg)
-		}
-		for _, v := range msgs {
-			attach, err := gmailService.Users.Messages.Attachments.Get("me", v.Id, v.Payload.Body.AttachmentId).Do()
-			log.Println("IDs:", v.Payload.Body.AttachmentId)
-			fmt.Println(attach.Data)
-			if err != nil {
-				log.Println(err)
+			msgAttachIds := getMessageAttachments(msg)
+
+			for _, attID := range msgAttachIds {
+				Attachement, err := gmailService.Users.Messages.Attachments.Get(a.Config.Username, v.Id, attID).Do()
+				if err != nil {
+					log.Println("Error getting the attachment %v of Message %v", attID, v.Id)
+				}
+				decoded, err := base64.URLEncoding.DecodeString(Attachement.Data)
+				if err != nil {
+					log.Println("ERROR IN DECODING ATTACHMENT")
+				}
+				// Just for testing
+				err = ioutil.WriteFile("Attachment", decoded, 0644)
+				if err != nil {
+					log.Printf("Failed to write attachment")
+				}
+
 			}
 
 		}
-
-		/*decoded_attachments := []byte{}
-		for _, v := range msgs {
-			attach, _ := gmailService.Users.Messages.Attachments.Get("me", v.Id, v.Payload.Body.AttachmentId).Do()
-			decoded, err := base64.URLEncoding.DecodeString(attach.Data)
-			if err != nil {
-				log.Println("Error", err)
-			}
-			decoded_attachments = append(decoded_attachments, decoded...)
-		}*/
 
 	}
 
+}
+
+/*
+// Get AttachmentIDs of multiple messages
+func getMessagesAttachments(messages []*gmail.Message) (attachIDS []string) {
+	attachId := []string{}
+	for _, v := range messages {
+		attchmsg := getMessageAttachments(v)
+		if len(attchmsg) > 0 {
+			for k, _ := range attchmsg {
+				attachId = append(attachId, attchmsg[k])
+			}
+		}
+	}
+	return attachId
+}*/
+
+// Get AttachmentIDs of a single Message Body
+func getMessageAttachments(message *gmail.Message) (attachIDs []string) {
+	var parts = message.Payload.Parts
+	attachId := []string{}
+	for _, v := range parts {
+		if v.Filename != "" && len(v.Filename) > 0 {
+			attachId = append(attachId, v.Body.AttachmentId)
+		}
+	}
+	return attachId
 }
